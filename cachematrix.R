8dab7e074784431e7448c14454f7f9d8b67e6deb
Nononox <taor@umich.edu>
2015-10-25 12:35:32 -0400

    Functions added

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+## by Ran Tao 2015/10/24
 
-## Write a short comment describing this function
+## The function makeCacheMatrix performs the set, get, setsolve and getsolve function in a list.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL;
+    set <- function(y) {
+        x <<- y;
+        m <<- NULL;
+    };
+    get <- function() x;
+    setsolve <- function(solve) m <<- solve;
+    getsolve <- function() m;
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve);
 }
 
 
@@ -12,4 +20,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data");
+        return(m);
+    };
+    data <- x$get();
+    m <- solve(data, ...);
+    x$setsolve(m);
+    m;
 }
+
+## For Test Use
+# a <- makeCacheMatrix()
+# ma <- matrix(1:16, 4, 4)
+# a$set(ma)
+# det(ma)
+# cacheSolve(a)
